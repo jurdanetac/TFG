3606f98131ee885c446819400622c5a25b9471ce
Juan Urdaneta <jurdanetac10@gmail.com>
2025-04-15 21:34:24 -0400

    Demo de pantalla

--- cliente/src/app/page.js
+++ cliente/src/app/page.js
@@ -1,95 +1,32 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import {Container, Button} from "react-bootstrap";
+
+const probarBackend = async () => {
+    const response = await fetch("http://localhost:5000/api/");
+    const data = await response.json();
+    console.log(data);
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <>
+            <header className={styles.header}>header</header>
+            <Container>
+                <main>
+                    <h1 className="text-center mb-4">Bienvenido, Usuario Regular</h1>
+                    <Button onClick={probarBackend}>Probar backend</Button>
+                    <div className="d-flex justify-content-center mb-4">
+                        <div className={styles.tarjeta}>
+                            1
+                        </div>
+                        <div className={styles.tarjeta}>
+                            2
+                        </div>
+                    </div>
+                </main>
+            </Container>
+        </>
+    );
 }
