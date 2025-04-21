09147db5a11bd0fc0dbffcb8db1744e3e5e32933
Juan Urdaneta <jurdanetac10@gmail.com>
2025-04-20 20:18:27 -0400

    Funcionalidad para subir un archivo y loggear b64

---
+++ cliente/pages/components/_Hero.jsx
@@ -0,0 +1,88 @@
+import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import { useState } from "react";
+
+
+export default function Hero() {
+    const [documento, setDocumento] = useState(null);
+    const [docBase64, setDocBase64] = useState(null);
+
+    // funcion para convertir archivo a base64
+    const toBase64 = (file) => new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = reject;
+    });
+
+    // funcion para setear el archivo al estar seleccionado
+    // se usa el evento onChange del input file
+    const handleFileChange = (e) => {
+        if (e.target.files) {
+            setDocumento(e.target.files[0]);
+        }
+    };
+
+    // funcion para subir el archivo en si y convertirlo a base64
+    const handleUpload = async () => {
+        const documentoInput = document.getElementById("documentoInput");
+        const documento = documentoInput.files[0];
+
+        // convertir a base64
+        const base64 = await toBase64(documento);
+        setDocBase64(base64);
+        console.log(base64);
+    };
+
+    return (
+        <Container className="px-4 py-5 my-5 text-center">
+            <Image
+                className="d-block mx-auto mb-4"
+                src="/bootstrap-logo.svg"
+                alt=""
+                width="72"
+                height="57"
+            />
+
+            <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+
+            <Row className="justify-content-center">
+                <Col lg={6}>
+                    <p className="lead mb-4">
+                        Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most
+                        popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid
+                        system, extensive prebuilt components, and powerful JavaScript plugins.
+                    </p>
+                    <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+
+                        {/* Input para subir el archivo */}
+                        <Form.Group className="mb-3">
+                            <Form.Control type="file" id="documentoInput" onChange={handleFileChange} />
+                        </Form.Group>
+
+                        {/* Detalles del archivo */}
+                        {documento && (
+                            <div>
+                                <h5>Detalles del archivo seleccionado:</h5>
+                                <ul>
+                                    <li><strong>Nombre:</strong> {documento.name}</li>
+                                    <li><strong>Tipo:</strong> {documento.type}</li>
+                                    <li><strong>Tamaño:</strong> {documento.size} bytes</li>
+                                </ul>
+                            </div>
+                        )}
+
+                        {/* Boton para subir el archivo */}
+                        {documento && (
+                            <Button
+                                onClick={handleUpload}
+                                variant="primary"
+                            >
+                                Subir
+                            </Button>
+                        )}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
