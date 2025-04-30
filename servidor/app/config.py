6b9430f28c97eee29fc6cd395e6c2ebad6aa7e8c
Juan Urdaneta <jurdanetac10@gmail.com>
2025-04-29 20:48:14 -0400

    Agregar secret key

--- servidor/app/config.py
+++ servidor/app/config.py
@@ -1,11 +1,21 @@
-class Config:
-    PORT = 5000
+import os
 
+
+class Config:
+    # Configuración de la aplicación
+    PORT = 5000
+    SECRET_KEY = os.urandom(24)
+
+    # Configuración de la base de datos
     HOST = "localhost"
     DB = "tfg"
     USUARIO = "postgres"
     CONTRASENA = "postgres"
 
+    # Configuración de rutas
+    # Cambiar esto según sistema operativo y la ruta donde almacenar los documentos
+    USUARIO_WINDOWS = "W10"
     DB_URI = f"postgresql://{USUARIO}:{CONTRASENA}@{HOST}/{DB}"
-
-    RUTA_DOCUMENTOS = f"C:/Users/W10/Documents/GitHub/TFG/servidor/documentos"
+    RUTA_DOCUMENTOS = (
+        f"C:/Users/{USUARIO_WINDOWS}/Documents/GitHub/TFG/servidor/documentos"
+    )
